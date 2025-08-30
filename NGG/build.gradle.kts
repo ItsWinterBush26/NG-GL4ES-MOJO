fe9d44ed106431b3c92fabde70cef8548d971af9
ItsWinterBush <154499206+ItsWinterBush26@users.noreply.github.com>
2025-08-30 16:24:55 +0800

    Update build.gradle.kts

--- NGG/build.gradle.kts
+++ NGG/build.gradle.kts
@@ -11,30 +11,38 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
-        ndkVersion = "28.2.13676358"
+
+        // Correct placement
+        ndk {
+            abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86", "x86_64")
+        }
     }
+
+    ndkVersion = "28.2.13676358"
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
         create("proguard") {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             initWith(getByName("debug"))
         }
         create("fordebug") {
         }
     }
+
     externalNativeBuild {
         cmake {
             path = file("NG-GL4ES/CMakeLists.txt")
             version = "3.22.1"
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
